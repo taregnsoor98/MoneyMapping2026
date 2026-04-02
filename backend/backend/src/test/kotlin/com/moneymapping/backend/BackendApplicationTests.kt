1cb800941407c99ae688dd8f47101adc0200f7be
Tariq <taregnoway2@gmail.com>
2026-04-02 15:08:58 +0300

    Initial commit: android and backend folders

---
+++ backend/backend/src/test/kotlin/com/moneymapping/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.moneymapping.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
